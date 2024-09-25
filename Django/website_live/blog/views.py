6391c80040f179ef056488c370be295f5a005449
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-09-25 15:22:44 -0400

    Finished demo in class

--- Django/website_live/blog/views.py
+++ Django/website_live/blog/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
 
-from blog.models import User
+from blog.models import User, Post
 
 
 def index(request):
@@ -44,34 +44,61 @@ def login(request):
         # Create a model instance and populate it with data from the request
         uname = request.POST["username"]
         pwd = request.POST["password"]
-        hashed_password = make_password(pwd)
-        user = User.objects.raw(f"SELECT * FROM blog_user WHERE username='{uname}' AND password='{hashed_password}'")
+        user = User.objects.filter(username=uname)
+
         if len(user) > 0:
-            # create a new session
-            request.session["user"] = uname
-            return HttpResponseRedirect(reverse('blog:list_posts'))
+            user_obj = user[0]
+            print("user obj", user_obj)
+            if check_password(pwd, user_obj.password):
+                # create a new session
+                request.session["user"] = uname
+                return HttpResponseRedirect(reverse('blog:list_posts'))
         else:
-            errors = [('authentication', "Login error")]
+            errors = [('authentication', "Username & pwd combination doesnt match the records")]
 
     return render(request, 'blog/login.html', {'errors': errors})
 
 
-
 # TODO: sign out feature (delete data from request.session, redirect to login)
 def logout(request):
-    return HttpResponse("logout TBD")
+    del request.session["user"]
+    return HttpResponseRedirect(reverse('blog:login'))
 
 
 # TODO: create post feature
 def create_post(request):
-    return HttpResponse("create post TBD")
+    if request.session.get("user") is None:
+        return HttpResponseRedirect(reverse("blog:login"))
+    if request.POST:
+        title = request.POST["title"]
+        content = request.POST["content"]
+        logged_user = User.objects.filter(username=request.session["user"])[0]
+        post = Post(
+            title=title,
+            content=content,
+            user=logged_user
+        )
+        post.save()
+        return HttpResponseRedirect(reverse("blog:list_posts"))
+
+    return render(request, "blog/create.html")
 
 
 # TODO: list all posts
 def list_posts(request):
-    return HttpResponse("list all posts TBD")
+    all_post = Post.objects.all()
+    return render(request, "blog/list.html", {
+        "posts": all_post
+    })
 
 
 # TODO: view a specific post
 def view_post(request, post_id):
+    p = Post.objects.filter(id=post_id)
+    if len(p) > 0:
+        post = p[0]
+        return render(request, "blog/view.html", {
+            "post": post
+        })
+
     return HttpResponse("view post TBD")
--- Django/website_live/website_live/settings.py
+++ Django/website_live/website_live/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m!y*)2^7gg$gak1h-o2hulm1$=at^n0e1de3+muzu_f5^5y+om'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+
+]
 
 
 # Application definition
